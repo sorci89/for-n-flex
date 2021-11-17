5107c860f84abfe26d4b91e928514211fb912c10
sorci89 <sore.eszter@gmail.com>
2021-11-17 14:51:45 +0100

    11.17. during class, flex css, for script

---
+++ script.js
@@ -0,0 +1,67 @@
+//generateArray();  //változóba mentett függvény nem hívható meg korábban
+
+const generateArray = function (amount){
+    let returnArray = [];
+    /*
+    let amountConverted = parseInt(amount);
+    amount = amountConverted
+    */
+
+    amount = parseInt(amount);
+    //console.log(amount);
+    //console.log(typeof amount);
+
+    if (!Number.isNaN(amount)) { // másik megoldás: Number.isNaN(amount) !== true
+        for (let i = 0; i < amount; i++) {
+            returnArray.push(`${i}`);
+            //console.log(returnArray);
+        }
+
+    } else {
+        returnArray.push("Error");
+      
+    }    
+    //console.log(returnArray);
+    return returnArray;
+}
+
+function loadEvent() {
+  /*
+  console.log(generateArray("kiskutya"));
+  console.log(generateArray("1 kiskutya"));
+  console.log(generateArray(100));
+  console.log(generateArray("100"));
+  console.log(generateArray([100]));
+  */
+
+  const root = document.getElementById("root");
+  const list = generateArray(600);
+
+  console.log(list);
+
+  if (list[0] !== "Error") {
+      for (const item of list) {
+          root.insertAdjacentHTML("beforeend", `<div>${item}</div>`)
+      }
+  }
+
+};
+
+//loadEvent(); // meg lehet hívni a load függvényt előtte
+
+window.addEventListener("load", loadEvent);
+
+/*
+window.addEventListener("load", function(){
+    ez is egy valid megoldás
+}   
+
+window.addEventListener("load", () => {
+    arrowfunctionnal valid megoldás
+});
+
+window.addEventListener("load", _ => {
+arrowfunction aláhúzással () helyett
+});
+
+*/
